6b6fb9bd80a6981ea23b2126e7ca0e20e9bd8a5d
dev <devendraverma9119@gmail.com>
2021-07-31 15:21:03 +0530

    modified main

--- EMS/src/com/mph/view/MainClass.java
+++ EMS/src/com/mph/view/MainClass.java
@@ -3,6 +3,7 @@ package com.mph.view;
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Tour Package EMS");
+		System.out.println("Welcome EMS");
 	}
 
 }
