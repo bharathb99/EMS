29011f0e7cae289c98079799abf3bb6963f33fca
chiranjeevi vangala <chiranjeevi3212@gmail.com>
2021-07-31 13:21:49 +0530

    changed in employee.java and salary.java

--- EMS/src/com/mph/model/Employee.java
+++ EMS/src/com/mph/model/Employee.java
@@ -1,5 +1,76 @@
 package com.mph.model;
-
+import java.util.Comparator;
 public class Employee {
+	
+	private int empno;
+	private String empname;
+	private Salary sal;
+	private String dept;
+	
+	
+	public Employee()
+	{
+		System.out.println("employee constructor..");
+	}
+	
+	public Employee(int empno, String empname,Salary sal ,String dept)
+	{
+		System.out.println("from parameterised constructor");
+		this.empno = empno;
+		this.empname = empname;
+		this.sal = sal;
+		this.dept = dept;
+	}
+	
+	
+	
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	
+	public Salary getSal() {
+		return sal;
+	}
+
+	public void setSal(Salary sal) {
+		this.sal = sal;
+	}
+	
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empno=" + empno + ", empname=" + empname + ", sal=" + sal + ", dept=" + dept + "]";
+	}
+
+	public int compareTo(Employee e1) {
+		return this.empno - e1.empno;
+	}
+	
+	//Anonymous inner class
+	public static Comparator<Employee> nameComparator = new Comparator<Employee>() {
+		@Override
+		public int compare(Employee  e1,Employee  e2) {
+			return (e1.getEmpname().compareTo(e2.getEmpname()));
+		}	
+		
+	};
+	
+		
 
 }
--- EMS/src/com/mph/model/Salary.java
+++ EMS/src/com/mph/model/Salary.java
@@ -1,5 +1,57 @@
 package com.mph.model;
+import com.mph.model.Salary;
 
 public class Salary {
-	private int  basic;
+	private int basic;
+	private int da;
+	private int hrf;
+	private int pf;
+	private int grossSalary;
+	private int netSalary;
+	
+
+	public int getBasic() {
+		return basic;
+	}
+	public void setBasic(int basic) {
+		this.basic = basic;
+		
+	}
+	public int getDa() {
+		return da;
+	}
+	public void setDa(int basic) {
+		this.da = basic*50/100;
+	}
+	public int getHrf() {
+		return hrf;
+	}
+	
+	public void setHrf(int basic) {
+		this.hrf = basic*40/100;
+	}
+	public int getPf() {
+		return pf;
+	}
+	public void setPf(int basic) {
+		this.pf = basic*10/100;
+	}
+	public int getGrossSalary() {
+		return grossSalary;
+	}
+	public void setGrossSalary() {
+		this.grossSalary = basic + da + hrf + pf;
+	}
+	public int getNetSalary() {
+		return netSalary;
+	}
+	public void setNetSalary() {
+		this.netSalary = grossSalary-pf;
+	}
+	
+	@Override
+	public String toString() {
+		return "Salary [basic=" + basic + ", da=" + da + ", hrf=" + hrf + ", pf=" + pf + ", grossSalary=" + grossSalary
+				+ ", netSalary=" + netSalary + "]";
+	}
 }
