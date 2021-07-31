e3e1fa7cef7bb5f4f03d76c76261016097ed52ba
bharath99 <bharathbr12@gmail.com>
2021-07-31 12:49:29 +0530

    Commit 16

--- EMS/src/com/mph/view/MainClass.java
+++ EMS/src/com/mph/view/MainClass.java
@@ -2,7 +2,7 @@ package com.mph.view;
 
 public class MainClass {
 	public static void main(String[] args) {
-		System.out.println("Welcome To EMS!!!");
+		System.out.println("Welcome To Tour Package EMS");
 	}
 
 }
