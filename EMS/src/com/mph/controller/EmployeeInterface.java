1d58c49652c50b2199adb008a1afdc701f93e336
Dhanu0412 <dhanushya0412@gmail.com>
2021-07-31 12:55:10 +0530

    Adding Interface Commit

--- EMS/src/com/mph/controller/EmployeeInterface.java
+++ EMS/src/com/mph/controller/EmployeeInterface.java
@@ -1,5 +1,9 @@
 package com.mph.controller;
+import com.mph.model.Employee;
 
-public class EmployeeInterface {
+import java.util.List;
+public interface EmployeeInterface {
+	public List addEmployee();
 
+	public void viewEmployee(List emlist);
 }
