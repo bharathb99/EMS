e97debc08cf18518cf83a7f2a30e67a08c612935
bharath99 <bharathbr12@gmail.com>
2021-07-31 15:04:21 +0530

    Commit 21

--- EMS/src/com/mph/controller/EmployeeController.java
+++ EMS/src/com/mph/controller/EmployeeController.java
@@ -1,5 +1,59 @@
 package com.mph.controller;
 
-public class EmployeeController {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+//import com.mph.daoimpl.EmployeeDao;
+//import com.mph.daoimpl.EmployeeDaoImpl;
+import com.mph.model.Employee;
+import com.mph.model.Salary;
+
+public class EmployeeController implements EmployeeInterface {
+
+	Employee emp;
+//	EmployeeDao dao = new EmployeeDaoImpl();
+	List elist = new ArrayList();
+
+	public List addEmployee() {
+
+		emp = new Employee();
+
+		System.out.println("\nAdd an Employee");
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter your Employee Number:");
+		int eno = sc.nextInt();
+		//emp.setEmpno(eno);
+
+		System.out.println("Enter Employee Name :");
+		//emp.setEmpname(sc.next());
+		System.out.println("Enter Employee Department :");
+		//emp.setDept(sc.next());
+
+		Salary sal = new Salary();
+		System.out.println("Enter Basic:");
+		int basic = sc.nextInt();
+		sal.setBasic(basic);
+		sal.setDa(basic);
+		//sal.setHra(basic);
+		sal.setPf(basic);
+		//sal.setGrossSalary(basic);
+		//sal.setNetSalary(basic);
+		//emp.setSal(sal);
+		elist.add(emp);
+		System.out.println("Employee Added Sucessfully !!!");
+		//dao.createEmployee(emp);
+		//dao.createEmployee(sal);
+		return elist;
+
+	}
+
+	@Override
+	public void viewEmployee(List emlist) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
