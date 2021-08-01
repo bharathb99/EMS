afdbe0970b2fa2608cc5e9b9d9ca4e7c421be4a9
prudvi27012000@gmail.com <prudvi27012000@gmail.com>
2021-08-01 22:38:05 +0530

    main merged

--- EMS/src/com/mph/view/MainClass.java
+++ EMS/src/com/mph/view/MainClass.java
@@ -1,23 +1,19 @@
 package com.mph.view;
 
-import com.mph.exception.*;
-
+import java.util.List;
 import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.function.BiPredicate;
 
-import com.mph.controller.*;
+import com.mph.controller.EmployeeController;
+import com.mph.controller.EmployeeInterface;
 import com.mph.exception.UserNotFoundException;
 
 public class MainClass {
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		System.out.println("Welcome To Tour Package EMS");
-		System.out.println("Welcome EMS");
-		System.out.println("Prudvi");
-=======
 
 	public static boolean userValidation()  throws IOException{
-		String uname = "tour", pass = "1234",un,pw;
+		String uname = "Dhanu", pass = "1129#",un,pw;
 		
 
 			InputStreamReader isr = new InputStreamReader(System.in);
@@ -33,7 +29,7 @@ public class MainClass {
 		if (un.equals(uname) && pw.equals(pass)) {
 				System.out.println("Please wait...");
 				try {
-					Thread.sleep(500);
+					Thread.sleep(5000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -43,7 +39,6 @@ public class MainClass {
 			return true;
 		} else
 			return false;
->>>>>>> a1f2382ed936a9495a42b5bdd3d8611cebadbc29
 	}
 	
 	public static void login() throws IOException, UserNotFoundException {
@@ -60,7 +55,16 @@ public class MainClass {
 					System.out.println("What do you want to do? \n Enter your Choice:");
 					System.out.println("1. Add Employee");
 					System.out.println("2. View Employee");
-					
+					System.out.println("3. Serialize Employee");
+					System.out.println("4. Deserialize Employee");
+					System.out.println("5. Sort Employee");
+					System.out.println("6. Group By Department");
+					System.out.println("7. Insert an employee");
+					System.out.println("8. Get MetaData");
+					System.out.println("9. Type Forward Only RS");
+					System.out.println("10. Type Scroll Insensitive RS");
+					System.out.println("11. Type Scroll sensitive RS");
+					System.out.println("12. Type Scroll sensitive UPDATE RS");
 					int choice = sc.nextInt();
 
 					switch (choice) {
@@ -72,24 +76,64 @@ public class MainClass {
 						ec.viewEmployee(elist);
 						break;
 					}
-					
+					case 3: {
+						ec.serializeEmployee(elist);
+						break;
+					}
+					case 4: {
+						ec.deSerializeEmployee(elist);
+						break;
+					}
+					case 5: {
+						ec.sortEmployee(elist);
+						break;
+					}
+					case 6: {
+						ec.groupByDeptartment(elist);
+						break;
+					}
+					case 7: {
+						ec.insertUsingProc();
+						break;
+					}
+					case 8: {
+						ec.rsmd();
+						break;
+					}
+					case 9: {
+						ec.type_forward_only_rs();
+						break;
+					}
+					case 10: {
+						ec.type_scroll_insensitive_rs();
+						break;
+					}
+					case 11: {
+						ec.type_scroll_sensitive_rs();
+						break;
+					}
+					case 12: {
+						ec.type_scroll_sensitive_update_rs();
+						break;
+					}
 					default:
-						System.out.println("plz enter a valid choice");
+						System.out.println("Plz enter a valid choice !!");
+						break;
+
 					}
 					System.out.println("Do you want to continue? Y or y");
 					input = sc.next();
 				} while (input.equals("Y") || input.equals("y"));
-				System.out.println("System Exited. Thanks You !");
+				System.out.println("System Exited. Thanks for using EMS !!!");
 				System.exit(0);
-				
 			} catch (InputMismatchException im) {
 				System.out.println(im);
 				login();
 			}
-		} else {
+		}else
+		{
 			throw new UserNotFoundException();
 		}
-
 	}
 
 	public static void main(String[] args) {
@@ -99,7 +143,8 @@ public class MainClass {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (UserNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
-}
