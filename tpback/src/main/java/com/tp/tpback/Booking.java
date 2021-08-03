8936c981da5f87f6ec377cf7604124a646171566
Dhanu0412 <dhanushya0412@gmail.com>
2021-08-04 00:31:50 +0530

    Comiiting pull

--- tpback/src/main/java/com/tp/tpback/Booking.java
+++ tpback/src/main/java/com/tp/tpback/Booking.java
@@ -1,5 +1,18 @@
 package com.tp.tpback;
 
-public class Booking {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
 
+@Entity
+public class Booking {
+	
+	@Id
+	@GeneratedValue
+	private int BookingID;
+	
+	@Column
+	private  Date BookingDate;
 }
