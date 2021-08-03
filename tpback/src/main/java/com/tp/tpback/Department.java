f1dbf00417a41a654a835cb97e5713e5220b5f4e
chiranjeevi vangala <chiranjeevi3212@gmail.com>
2021-08-03 22:27:20 +0530

    pulled from origin

--- tpback/src/main/java/com/tp/tpback/Department.java
+++ tpback/src/main/java/com/tp/tpback/Department.java
@@ -1,5 +1,9 @@
 package com.tp.tpback;
 
 public class Department {
-
+	
+	private String department_name;
+	private int department_number;
+	
+	
 }
--- tpback/src/main/java/com/tp/tpback/Employee.java
+++ tpback/src/main/java/com/tp/tpback/Employee.java
@@ -1,5 +1,63 @@
 package com.tp.tpback;
 
-public class Employee {
+import javax.persistence.*;
 
+@Entity
+public class Employee {
+	
+	private String name;
+	private int phone;
+	private String gender;
+	
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Employee(String name, int phone, String gender) {
+		super();
+		this.name = name;
+		this.phone = phone;
+		this.gender = gender;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", phone=" + phone + ", gender=" + gender + "]";
+	}
+
+	
 }
