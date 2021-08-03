e9f22eb2f1c5199cf6bda5df67d3501a2ea69c00
dev <devendraverma9119@gmail.com>
2021-08-03 19:53:00 +0530

    just checked package

--- tpback/src/main/java/com/tp/tpback/Package.java
+++ tpback/src/main/java/com/tp/tpback/Package.java
@@ -1,5 +1,11 @@
 package com.tp.tpback;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Package {
 
 }
