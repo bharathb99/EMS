733357d7320db836cb687f644764fc67e7f3d3d3
Dhanu0412 <dhanushya0412@gmail.com>
2021-08-03 16:47:13 +0530

    Hibernate Tour Package Backend Structure Creation

---
+++ tpback/src/main/java/com/tp/tpback/Employee.java
@@ -0,0 +1,5 @@
+package com.tp.tpback;
+
+public class Employee {
+
+}
---
+++ tpback/src/main/java/com/tp/tpback/TPMain.java
@@ -0,0 +1,10 @@
+package com.tp.tpback;
+
+public class TPMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
