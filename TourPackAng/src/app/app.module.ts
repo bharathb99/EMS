6cb40055a5fac1cbcc85b20ae9073b489f2a8663
bharath99 <bharathbr12@gmail.com>
2021-08-15 13:31:34 +0530

    Added Register page component

--- TourPackAng/src/app/app.module.ts
+++ TourPackAng/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterpageComponent } from './registerpage/registerpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterpageComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
